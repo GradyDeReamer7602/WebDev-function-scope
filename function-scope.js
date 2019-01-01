ed9618022030410041e9ccd7b9c6245dce3362e8
Bruce Johnson <bruce@yogainternational.com>
2019-01-01 13:20:25 -0500

    improved problem wording and added mocha and chai tests

--- function-scope.js
+++ function-scope.js
@@ -1,33 +1,36 @@
 //Using what you learned from the README, hypothesize what would happen in each of the following cases.
-//If the code will throw an error, explain why in a comment. Otherwise, explain what the result would be in
-//a comment. 
+//If the code will throw an error, run the corresponding function for that problem and pass in
+//'true'. Otherwise, run the corresponding function for that problem and pass in 'false'
 //Do NOT run the code... run it in your mind, instead. :)
 
-//1. Will this function throw an error or not? Explain your answer.
+//1. Would this console.log() throw an error or not?
 var multiplyTwoNums = function(factor1, factor2){
    return(factor1 * factor2);
 }
 multiplyTwoNums();
-console.log(factor1);
-//your comment here
+//console.log(factor1); 
 
-//2. Will this function throw an error or not? Explain your answer.
+answerOne = 'answer'; //Your answer here
+
+//2. Will this console.log() throw an error or not?
 var name = "Bruce";
 var returnName = function(nameArg){
    name = "Ryan";
    return(nameArg + name);
 }
 returnName("My name is ");
-console.log(name);
-//your comment here
+// console.log(name);
 
-//3. Will this function throw an error or not? Explain your answer. 
+answerTwo = 'answer'; //Your answer here
+
+//3. Will this console.log() throw an error or not?
 //*Don't get confused by the two ${ } on line 29. They are called template literals and simply allow
-//you to place variables into a string*
+//you to place variables into a string. We'll talk more about these later.*
 var pizzaType = "cheese"; 
 var pizzaMaker = function(customerName, kindOfPizza){
    return(`Your name is ${customerName} and you ordered a ${kindOfPizza} pizza.`);
 }
 pizzaMaker("Ryan", pizzaType);
-console.log(customerName, pizzaType);
-//your comment here
+// console.log(customerName, pizzaType);
+
+answerThree = 'answer'; //Your answer here
